ea9bf48043c7dc2d1dd9d50178233614e80590ff
yasmeen <yasmeentarek112001@gmail.com>
2024-01-25 13:52:10 +0200

    update style

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,7 +1,62 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
 export default function Footer() {
   return (
-    <div className='footer p-4 mt-3 '>copy right© reserved in 2024 </div>
-  )
+    <>
+      <div className="w-100 mx-auto mt-5">
+        <footer class="footer-background text-center text-white">
+          <img src="./images/FlaxEgyptFinal_-removebg-preview.png" width={"150px"} alt="logo"/>
+          <div class="container  pb-0">
+            <section class="mb-4">
+              <a
+                class="btn btn-primary btn-floating m-1"
+                style={{ backgroundColor: "#3b5998;" }}
+                href="#!"
+                role="button"
+              >
+                <i class="fab fa-facebook-f"></i>
+              </a>
+
+              <a
+                class="btn btn-primary btn-floating m-1"
+                style={{backgroundColor: "#55acee;"}}
+                href="#!"
+                role="button"
+              >
+                <i class="fab fa-twitter"></i>
+              </a>
+
+              <a
+                class="btn btn-primary btn-floating m-1"
+                style={{backgroundColor: "#dg4b39;"}}
+                href="#!"
+                role="button"
+              >
+                <i class="fab fa-google"></i>
+              </a>
+
+              <a
+                class="btn btn-primary btn-floating m-1"
+                style={{backgroundColor: "#ac2bac;"}}
+                href="#!"
+                role="button"
+              >
+                <i class="fab fa-instagram"></i>
+              </a>
+            </section>
+          </div>
+<hr className="w-75 mx-auto"></hr>
+          <div
+            class="text-center p-3"
+            style={{backgrounColor:"rgba(0, 0, 0, 0.2)"}}
+          >
+            © 2024 Copyright:
+            <a class="text-white" href="https://mdbootstrap.com/">
+             Egypt Flax.com
+            </a>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
 }
